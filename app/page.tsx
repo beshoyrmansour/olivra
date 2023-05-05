3d16f8ef2aa9a35cba056e52c6618e4210d58d29
Beshoy R Mansour <115085784+ib-bmansour@users.noreply.github.com>
2023-05-05 12:47:15 +0300

    Update card size

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,15 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main >
-      <div className='flex flex-center justify-center align-center w-full md:w-1/2'>
+    <main style={{ height: '100vh', width: '100vw' }}>
+      <div className='flex flex-center justify-center align-center h-full w-full md:w-1/2 m-auto' >
         <Image
           className="relative "
           src="/card.svg"
           alt="olivra Logo"
-
-          fill
+          width={100}
+          height={100}
+          style={{ height: '100%', width: '100%', }}
           priority
         />
       </div>
